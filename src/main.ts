6f2144e19830f62ccdd0516a271b1e50ed0c1d0b
Sebastian barrera <sebasbh2008@hotmail.com>
2025-01-17 05:28:17 -0500

    style: :lipstick: update styles

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,34 @@
 
 
+const buttons = document.querySelectorAll<HTMLButtonElement>('.services__button');
+const images = document.querySelectorAll<HTMLImageElement>('.services__image');
+const drowpdown = document.querySelector<HTMLElement>('.dropdown');
+const drowTeams = document.getElementById('menu-dropdown');
+
+buttons.forEach((button, index) => {
+
+  button.addEventListener('click', () => {
+    buttons.forEach(btn => {
+      btn.classList.remove('active');
+      btn.style.backgroundColor = "";
+    });
+
+    images.forEach(image => image.classList.remove('active'));
+
+    button.classList.add('active');
+    button.style.backgroundColor = "#e5e5e5";
+
+    if (images[index]) {
+      images[index].classList.add('active');
+    }
+
+    console.log(index);
+    console.log("funcionaa")
+  });
+});
+
+drowTeams?.addEventListener('click', () => {
+  const visibilidad = drowpdown!.style.visibility;
+  drowpdown!.style.visibility = visibilidad === "visible" ? "hidden" : "visible";
+});
+
